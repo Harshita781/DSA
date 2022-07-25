267ea134de644df50a96159673c1f040f2acc4d9
Harshita781 <sharmaharshita431@gmail.com>
2022-07-25 21:28:43 +0530

    initial

--- Array/KthLargestSumSubarray.cpp
+++ Array/KthLargestSumSubarray.cpp
@@ -1,3 +1,16 @@
+/*
+You have been given an array/list of ‘N’ integers. Now you are supposed to find the K-th largest sum of the subarray.
+Please note that a subarray is the sequence of consecutive elements of the array.
+Sample Input 1 :
+2
+3 3
+3 -2 5
+2 2
+4 1
+Sample output 1 :
+3
+4
+*/
 #include<bits/stdc++.h>
 #include<queue>
 using namespace std;
