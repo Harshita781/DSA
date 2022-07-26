ed5415319ceed1862220229208089fabc9e145c3
Harshita781 <sharmaharshita431@gmail.com>
2022-07-26 22:33:40 +0530

    initial

--- Linked List/Reverse.cpp
+++ Linked List/Reverse.cpp
@@ -1,72 +1,22 @@
-#include<bits/stdc++.h>
-using namespace std;
-class Node {
-    public:
-    int data;
-    Node* next;
-
-    //constructor
-    Node(int data) {
-        this -> data = data;
-        this -> next = NULL;
-    }
-
-    //destructor
-    ~Node() {
-        int value = this -> data;
-        //memory free
-        if(this->next != NULL) {
-            delete next;
-            this->next = NULL;
-        }
-        cout << " memory is free for node with data " << value << endl;
-    }
-
-};
-void insertAtTail(Node* &tail, int d) {
-     // new node create
-    Node* temp = new Node(d);
-    tail -> next = temp;
-    tail  = temp;
-}
-
-Node* Reverse_ll(Node *head){
+/*
+Given pointer to the head node of a linked list, the task is to reverse the linked list. We need to reverse the list by changing the links between nodes.
+Input: Head of following linked list 
+1->2->3->4->NULL 
+Output: Linked list should be changed to, 
+4->3->2->1->NULL
+*/
+Node* Reverse_ll(Node* head){
     if(head==NULL || head->next==NULL){
-        return NULL;
+        return head;
     }
-      Node* temp1=head;
-      Node* temp2=temp1;
-      while(temp2!=NULL){
-        temp2=temp1->next->next;
-        temp1=temp1->next;       
-      }
-      return temp1;
-}
-void print(Node* &head) {
-
-    if(head == NULL) {
-        cout << "List is empty "<< endl;
-        return ;
+    Node* next=NULL;
+    Node* curr=head;
+    Node* prev=NULL;
+    while(curr!=NULL){
+        next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
     }
-    Node* temp = head;
-
-    while(temp != NULL ) {
-        cout << temp -> data << " ";
-        temp = temp -> next;
-    }
-    cout << endl;
-}
-
-int main() {
-    
-    Node* node1 = new Node(10);
-    Node* head = node1; 
-    Node* tail = node1;
-    insertAtTail(tail, 15);
-    insertAtTail(tail,20);
-    insertAtTail(tail,25);
-    insertAtTail(tail,30);
-    Reverse_ll(head);
-    print(head);
-    return 0;
+    return prev;
 }
---
+++ Linked List/ReverseKthGroup.cpp
@@ -0,0 +1,33 @@
+/*
+Problem Statement
+You are given a linked list of 'N' nodes and an integer 'K'. You have to reverse the given linked list in groups of size K i.e if the list contains x nodes numbered from 1 to x, then you need to reverse each of the groups (1,K),(K+1,2*K), and so on.
+For example, if the list is [1, 2, 3, 4, 5, 6] and K = 2, then the new list will be [2, 1, 4, 3, 6, 5].
+Sample Input 1:
+2
+1 2 3 4 5 6 -1
+2
+5 4 3 7 9 2 -1
+4 
+Sample Output 1:
+2 1 4 3 6 5
+7 3 4 5 2 9
+*/
+Node* KReverse(Node* head,int k){
+    if(head==NULL){
+        return NULL;
+    }
+    Node* next=NULL;
+    Node* curr=head;
+    Node* prev=NULL;
+    int count=0;
+    while(curr!=NULL && count<k){
+        next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
+    }
+    if(next!=NULL){
+        head->next=KReverse(next,k);
+    }
+    return prev;
+}
