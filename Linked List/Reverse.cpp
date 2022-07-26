e9025fa3d0dc5a4c153b6b3c5e8c8b392bb82d36
Harshita781 <sharmaharshita431@gmail.com>
2022-07-26 17:09:17 +0530

    initial

--- Linked List/Reverse.cpp
+++ Linked List/Reverse.cpp
@@ -1,69 +1,72 @@
 #include<bits/stdc++.h>
 using namespace std;
-int highestPowerof(int n){
-    int ans=0;
-    for(int i=n;i>=1;i--){
-        if((i&(i-1))==0){
-            ans=i;
-            break;
-        }
+class Node {
+    public:
+    int data;
+    Node* next;
+
+    //constructor
+    Node(int data) {
+        this -> data = data;
+        this -> next = NULL;
     }
-    return ans;
+
+    //destructor
+    ~Node() {
+        int value = this -> data;
+        //memory free
+        if(this->next != NULL) {
+            delete next;
+            this->next = NULL;
+        }
+        cout << " memory is free for node with data " << value << endl;
+    }
+
+};
+void insertAtTail(Node* &tail, int d) {
+     // new node create
+    Node* temp = new Node(d);
+    tail -> next = temp;
+    tail  = temp;
 }
+
+Node* Reverse_ll(Node *head){
+    if(head==NULL || head->next==NULL){
+        return NULL;
+    }
+      Node* temp1=head;
+      Node* temp2=temp1;
+      while(temp2!=NULL){
+        temp2=temp1->next->next;
+        temp1=temp1->next;       
+      }
+      return temp1;
+}
+void print(Node* &head) {
+
+    if(head == NULL) {
+        cout << "List is empty "<< endl;
+        return ;
+    }
+    Node* temp = head;
+
+    while(temp != NULL ) {
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout << endl;
+}
+
 int main() {
-    int t;
-    cin>>t;
-    while(t--){
-        long n;
-        cin>>n;
-        long *arr= new long[n+1];
-        for(int i=0;i<=n;i++){
-            cin>>arr[i];
-        }
-    long long res[3]={0,0,0};
-    for(int i=highestPowerof(n);i>0;i/=2){
-        long diff=arr[i]-arr[0];
-        if(diff<0){
-            diff*=-1;
-            int ct=diff/i;
-            if(ct==1){
-                ct=2;
-                if((res[0]^i)<=n){
-                    res[0]^=i;
-                    ct--;
-                }
-                if(ct && (res[1]^i)<=n){
-                    res[1]^=i;
-                    ct--;
-                }
-                if(ct>0 && (res[2]^i)<=n){
-                    res[2]^=i;
-                }
-            }
-            else if(ct==3){
-                res[0]^=i;
-                res[1]^=i;
-                res[2]^=i;
-            }
-        }
-        else{
-            int ct=diff/i;
-            if(ct==1){
-                if((res[0]^i)<=n){
-                    res[0]^=i;
-                }
-                else if((res[1]^i)<=n){
-                    res[1]^=i;
-                }
-                else{
-                    res[2]^=i;
-                }
-            }
-        }
-        sort(res,res+3);
-    }
-    cout<<res[0]<<res[1]<<res[2]<<endl;
-    }
-	// your code goes here
-	return 0;
+    
+    Node* node1 = new Node(10);
+    Node* head = node1; 
+    Node* tail = node1;
+    insertAtTail(tail, 15);
+    insertAtTail(tail,20);
+    insertAtTail(tail,25);
+    insertAtTail(tail,30);
+    Reverse_ll(head);
+    print(head);
+    return 0;
 }
