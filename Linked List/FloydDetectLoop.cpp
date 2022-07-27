3ccbc88027f1ca7cbed077a4723558acae78b755
Harshita781 <sharmaharshita431@gmail.com>
2022-07-27 22:52:53 +0530

    initial

---
+++ Linked List/FloydDetectLoop.cpp
@@ -0,0 +1,20 @@
+Node* floydDetectLoop(Node* head){
+    if(head==NULL){
+        return false;
+    }
+    Node* slow=head;
+    Node* fast=head;
+    while(slow!=NULL && fast!=NULL){
+        fast=fast->next;
+        if(fast!=NULL){
+            fast->next;
+
+        }
+        slow=slow->next;
+        if(slow==fast){
+            return true;
+        }
+
+    }
+    return false;
+}
