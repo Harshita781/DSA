6bfbabf001365e690e7b4a0385cb998135a0f073
Harshita781 <sharmaharshita431@gmail.com>
2022-07-28 19:03:53 +0530

    initial

--- Linked List/RemoveDuplicates.cpp
+++ Linked List/RemoveDuplicates.cpp
@@ -1,4 +1,16 @@
+/*
+You want to play your fried, but your friend has pending homework, help him complete that.
+Yoy are given a 'Head' of a sorted linked list.You can remove some values from the linked list.In the end,you must return a sorted linked list that contains no adjacent values that are equal.
 
+Sample Input 1 :
+2
+1 2 2 3 -1
+1 2 3 4 -1
+Sample Output 1 :
+1 2 3 -1
+1 2 3 4 -1
+
+*/
 Node* uniqueSortedList(Node* head){
     if(head==NULL){
         return NULL;
