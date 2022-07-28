c475502ab0cc8e605efe8a915dc2a4d41b973773
Harshita781 <sharmaharshita431@gmail.com>
2022-07-28 10:53:14 +0530

    initial

--- Linked List/RemoveLoop.cpp
+++ Linked List/RemoveLoop.cpp
@@ -1,3 +1,15 @@
+/*
+Problem Statement
+Given a singly linked list, you have to detect the loop and remove the loop from the linked list, if present.You have to make changes in the given linked list itself and return the updated linked list.
+
+Expected Complexity:Try doing it in O(n) time complexity and O(1) space complexity.Here, n is the number of node in the linked list.
+
+Sample Input:
+6 2
+1 2 3 4 5 6 
+Sample Output:
+1 2 3 4 5 6
+*/
 Node* floydDetectLoop(Node* head){
     if(head==NULL){
         return NULL;
