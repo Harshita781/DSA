225936a44b7769383e10bf4430aedff8720bbe82
Harshita781 <sharmaharshita431@gmail.com>
2022-07-30 21:56:41 +0530

    initial

--- Linked List/Sort_0's_1's_2's.cpp
+++ Linked List/Sort_0's_1's_2's.cpp
@@ -9,7 +9,7 @@ Sample Output 1:
 0 1 1 2 2 -1
 0 0 1 2 -1
 */
-
+/*
 Node* Sort(Node* head){
     int zeroCount=0;
     int oneCount=0;
@@ -43,5 +43,41 @@ Node* Sort(Node* head){
         }
         temp=temp->next;
     }
+    return head;
+}
+*/
+//Approach 2
+Node* SortList(Node* head){
+    Node* zerohead=new Node(-1);
+    Node* zeroTail=zerohead;
+    Node* onehead=new Node(-1);
+    Node* oneTail=zerohead;
+    Node* twohead=new Node(-1);
+    Node* twoTail=twohead;
+    Node* curr=head;
+    while(curr!=NULL){
+        int value=curr->data;
+        if(value==0){
+            insertAtTail(zeroTail,curr);
+
+        }
+        else if(value==1){
+            insertAtTail(oneTail,curr);
+
+        }
+        curr=curr->next;
+    }
+    if(onehead->next!=NULL){
+        zeroTail->next=onehead->next;
+    }
+    else {
+        zeroTail->next=twohead->next;
+    }
+    oneTail->next=twohead->next;
+    twoTail->next=NULL;
+    head=zerohead->next;
+    delete zerohead;
+    delete onehead;
+    delete twohead;
     return head;
 }
