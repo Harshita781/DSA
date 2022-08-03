eb353eb92732c5ceb592ab7df6b45cfcbf202ffc
Harshita781 <sharmaharshita431@gmail.com>
2022-08-03 22:59:03 +0530

    initial

---
+++ Linked List/CheckPallindrome.cpp
@@ -0,0 +1,23 @@
+bool checkPalindrome(vector<int> arr){
+    int n=arr.size();
+    int s=0;
+    int e=n-1;
+    while(s<=e){
+        if(arr[s]!=arr[e]){
+            return 0;
+        }
+        s++;
+        e--;
+    }
+    return 1;
+}
+
+bool IsPallindrome(Node* head){
+    vector<int> arr;
+    Node* temp=head;
+    while(temp!=NULL){
+        arr.push_back(temp->data);
+        temp=temp->next;
+    }
+    return checkPallindrome(arr);
+}
