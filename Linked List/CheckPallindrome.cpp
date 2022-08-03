246041b1a0832ae4a312af2ef3f9028d279d769f
Harshita781 <sharmaharshita431@gmail.com>
2022-08-03 23:49:14 +0530

    initial

--- Linked List/CheckPallindrome.cpp
+++ Linked List/CheckPallindrome.cpp
@@ -7,7 +7,7 @@ N = 3
 value[] = {1,2,1}
 Output: 1
 */
-
+//Approach 1
 bool checkPalindrome(vector<int> arr){
     int n=arr.size();
     int s=0;
@@ -30,4 +30,49 @@ bool IsPallindrome(Node* head){
         temp=temp->next;
     }
     return checkPallindrome(arr);
+}
+//Approach 2
+Node* Middle(Node* head){
+    Node* slow=head;
+    Node* fast=head->next;
+    while(fast!=NULL && fast->next!=NULL){
+        fast=fast->next->next;
+        slow=slow->next;
+    }
+    return slow;
+}
+
+Node* reverse(Node* head){
+    Node* curr=head;
+    Node* prev=NULL;
+    Node* next=NULL;
+    while(curr!=NULL){
+        next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next
+    }
+    return prev;
+}
+
+bool IsPalindrome(Node* head){
+    if(head->next==NULL){
+        return true;
+    }
+    Node* middle=Middle(head);
+    Node* temp=middle->next;
+    middle->next=reverse(head);
+    Nod* head1=head;
+    Node* head2=middle->next;
+    while(head2!=NULL){
+        if(head->data!=head2->data){
+            return false;
+        }
+        head1=head1->next;
+        head2=head2->next;
+    }
+    temp=middle->next;
+    middle->next=reverse(temp);
+
+    return true;
 }
