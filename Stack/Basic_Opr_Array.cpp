7c8108a99b67a691a43760d3b84d6ef0a2cb4dbd
Harshita781 <sharmaharshita431@gmail.com>
2022-08-10 21:15:25 +0530

    format file

--- Stack/Basic_Opr_Array.cpp
+++ Stack/Basic_Opr_Array.cpp
@@ -1,59 +1,75 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Stack{
-    public:
+class Stack
+{
+public:
     int *arr;
     int size;
     int top;
-    Stack(int size){
-        this->size=size;
-        arr=new int[size];
-        top=-1;
+    Stack(int size)
+    {
+        this->size = size;
+        arr = new int[size];
+        top = -1;
     }
-    void push(int element){
-        if(size-top>1){
+    void push(int element)
+    {
+        if (size - top > 1)
+        {
             top++;
-            arr[top]=element;
+            arr[top] = element;
         }
-        else{
-            cout<<"Stack is full"<<endl;
+        else
+        {
+            cout << "Stack is full" << endl;
         }
     }
-      void pop(){
-        if(top>=0){
+    void pop()
+    {
+        if (top >= 0)
+        {
             top--;
         }
-        else{
-            cout<<"Stack is empty"<<endl;
+        else
+        {
+            cout << "Stack is empty" << endl;
         }
     }
-    int peek(){
-        if(top>=0){
-            cout<<arr[top]<<endl;
+    int peek()
+    {
+        if (top >= 0)
+        {
+            cout << arr[top] << endl;
         }
-        else{
-            cout<<"Stack is empty"<<endl;
+        else
+        {
+            cout << "Stack is empty" << endl;
         }
     }
-    bool empty(){
-        if(top==-1){
+    bool empty()
+    {
+        if (top == -1)
+        {
             return true;
         }
-        else{
+        else
+        {
             return false;
         }
     }
 };
 
-int main(){
+int main()
+{
     Stack s(4);
     s.push(10);
     s.push(20);
     s.peek();
     s.pop();
     s.pop();
-    if(s.empty()){
-        cout<<"Stack is empty"<<endl;
+    if (s.empty())
+    {
+        cout << "Stack is empty" << endl;
     }
     s.push(30);
     s.peek();
