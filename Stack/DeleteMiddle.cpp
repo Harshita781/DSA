7ea8c0ae798aac55920f851b6b087fec0bbea0af
Harshita781 <sharmaharshita431@gmail.com>
2022-08-15 22:00:01 +0530

    initial

--- Stack/DeleteMiddle.cpp
+++ Stack/DeleteMiddle.cpp
@@ -1,3 +1,17 @@
+/*
+Problem Statement
+You are having a stack "ARR" of size 'N+1',your task is to delete the middlemost element so that the size of resulting stack is 'N'.
+Sample Input 1:
+2
+4
+1 2 3 4 5
+7
+83 74 67 49 94 8 11 1
+Sample Output 1:
+1 2 4 5
+83 74 67 94 8 11 1
+*/
+
 #include<iostream>
 #include<stack>
 using namespace std;
@@ -15,7 +29,6 @@ void solve(stack<int>&ipstack,int count,int size){
 void Delete(stack<int>&ipstack,int n){
     int count=0;
     solve(ipstack,count,n);
-    cout<<ipstack.size()<<endl;
 }
 
 int main(){
