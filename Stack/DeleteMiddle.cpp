dad9b126fcb3ba48627dab2c93495b13f6162898
Harshita781 <sharmaharshita431@gmail.com>
2022-08-15 22:00:29 +0530

    initial

--- Stack/DeleteMiddle.cpp
+++ Stack/DeleteMiddle.cpp
@@ -12,33 +12,37 @@ Sample Output 1:
 83 74 67 94 8 11 1
 */
 
-#include<iostream>
-#include<stack>
+#include <iostream>
+#include <stack>
 using namespace std;
-void solve(stack<int>&ipstack,int count,int size){
-    if(count==size/2){
+void solve(stack<int> &ipstack, int count, int size)
+{
+    if (count == size / 2)
+    {
         ipstack.pop();
         return;
     }
-    int num=ipstack.top();
+    int num = ipstack.top();
     ipstack.pop();
-    solve(ipstack,count+1,size);
+    solve(ipstack, count + 1, size);
     ipstack.push(num);
 }
 
-void Delete(stack<int>&ipstack,int n){
-    int count=0;
-    solve(ipstack,count,n);
+void Delete(stack<int> &ipstack, int n)
+{
+    int count = 0;
+    solve(ipstack, count, n);
 }
 
-int main(){
-    stack<int>ipstack;
+int main()
+{
+    stack<int> ipstack;
     ipstack.push(10);
     ipstack.push(32);
     ipstack.push(12);
     ipstack.push(43);
     ipstack.push(11);
-    int n=ipstack.size();
-    Delete(ipstack,n);
+    int n = ipstack.size();
+    Delete(ipstack, n);
     return 0;
 }
