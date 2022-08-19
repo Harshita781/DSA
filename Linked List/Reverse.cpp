22ed94a9a5df8581274f2624f2a8e87875e899c6
Harshita781 <sharmaharshita431@gmail.com>
2022-08-19 22:12:40 +0530

    format file

--- Linked List/Reverse.cpp
+++ Linked List/Reverse.cpp
@@ -1,22 +1,25 @@
 /*
 Given pointer to the head node of a linked list, the task is to reverse the linked list. We need to reverse the list by changing the links between nodes.
-Input: Head of following linked list 
-1->2->3->4->NULL 
-Output: Linked list should be changed to, 
+Input: Head of following linked list
+1->2->3->4->NULL
+Output: Linked list should be changed to,
 4->3->2->1->NULL
 */
-Node* Reverse_ll(Node* head){
-    if(head==NULL || head->next==NULL){
+Node *Reverse_ll(Node *head)
+{
+    if (head == NULL || head->next == NULL)
+    {
         return head;
     }
-    Node* next=NULL;
-    Node* curr=head;
-    Node* prev=NULL;
-    while(curr!=NULL){
-        next=curr->next;
-        curr->next=prev;
-        prev=curr;
-        curr=next;
+    Node *next = NULL;
+    Node *curr = head;
+    Node *prev = NULL;
+    while (curr != NULL)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
     }
     return prev;
 }
