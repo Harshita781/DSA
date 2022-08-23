537ecfb84c8d02be86b2023f4a1234189c5a5892
Harshita781 <sharmaharshita431@gmail.com>
2022-08-23 18:04:51 +0530

    initial commit

---
+++ Stack/Reverse.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+void insertAtBottom(stack<int> &s,int element){
+    if(s.empty()){
+        s.push(element);
+        return;
+    }
+    int num=s.top();
+    s.pop();
+    insertAtBottom(s,element);
+    s.push(num);
+}
+void reverseStack(stack<int> &stack){
+    if(stack.empty()){
+        return;
+
+    }
+    int num=stack.top();
+    stack.pop();
+    reverseStack(stack);
+    insertAtBottom(stack,num);
+}
+
+int main(){
+    stack<int> stack;
+    stack.push(12);
+    stack.push(32);
+    stack.push(10);
+    stack.push(9);
+    cout<<stack.top()<<endl;
+    reverseStack(stack);
+    cout<<stack.top();
+    return 0;
+}
