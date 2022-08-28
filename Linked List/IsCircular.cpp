47562a8a425d5aecf97c6025be4366335780974d
Harshita781 <sharmaharshita431@gmail.com>
2022-08-28 21:00:00 +0530

    format file

--- Linked List/IsCircular.cpp
+++ Linked List/IsCircular.cpp
@@ -1,12 +1,16 @@
-bool IsCircular(Node* head){
-    if(head=NULL){
+bool IsCircular(Node *head)
+{
+    if (head = NULL)
+    {
         return true;
     }
-    Node* temp=head->next;
-    while(temp!=NULL && temp!=head){
-        temp=temp->next;
+    Node *temp = head->next;
+    while (temp != NULL && temp != head)
+    {
+        temp = temp->next;
     }
-    if(temp==head){
+    if (temp == head)
+    {
         return true;
     }
     return false;
--- Linked List/MergeSortedLL.cpp
+++ Linked List/MergeSortedLL.cpp
@@ -8,38 +8,47 @@ void solve(Node<int>* first, Node<int>* second){
     Node* next2=curr2->next;
 */
 
-    while(next1!=NULL && curr2!=NULL){
-        if((curr2->data>=curr2=!=NULL) && (curr2->data<=next1->data)){
-            curr1->next=curr2;
-            next2=curr2->next;
-            curr->next=next1;
+while (next1 != NULL && curr2 != NULL)
+{
+    if ((curr2->data >= curr2 = != NULL) && (curr2->data <= next1->data))
+    {
+        curr1->next = curr2;
+        next2 = curr2->next;
+        curr->next = next1;
 
-            curr1=curr2;
-            curr2=next2;
-        }
-        else{
-            curr1=next1;
-            next1=next1->next;
-            if(next1==NULL){
-                curr1->next=curr2;
-                return first;
-            } 
+        curr1 = curr2;
+        curr2 = next2;
+    }
+    else
+    {
+        curr1 = next1;
+        next1 = next1->next;
+        if (next1 == NULL)
+        {
+            curr1->next = curr2;
+            return first;
         }
     }
-    return first;
+}
+return first;
 }
 
-Node<int>* sortTwoLists(Node<int>* first, Node<int>* second){
-    if(first==NULL){
+Node<int> *sortTwoLists(Node<int> *first, Node<int> *second)
+{
+    if (first == NULL)
+    {
         return second;
     }
-    if(second==NULL){
+    if (second == NULL)
+    {
         return first;
     }
-    if(first->data<=second->data){
-        solve(first,second);
+    if (first->data <= second->data)
+    {
+        solve(first, second);
     }
-    else{
-        solve(second,first);
+    else
+    {
+        solve(second, first);
     }
 }
