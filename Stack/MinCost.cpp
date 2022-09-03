caa527c8fe24f99a0eb8083892c2bf21935a0705
Harshita781 <sharmaharshita431@gmail.com>
2022-09-03 20:40:37 +0530

    initial commit

--- Stack/MinCost.cpp
+++ Stack/MinCost.cpp
@@ -1,3 +1,16 @@
+/*
+Problem Statement
+You are given a string 'STR' containing either '{' or '}'. 'STR' is called valid if all the brackets are balanced. Formally for each opening bracket.ther must be a closing bracket right to it.
+You want to make 'STR' valid by performing some operations on it.In one operation,he can convert '{' into'}' or vice versa, and the cost of one such operation is 1.
+Your task is to determine the minimum cost to make 'STR' valid.
+Sample Input 1:
+2
+{{{}
+{{}{}}
+Sample Output 1:
+1
+0
+*/
 #include<iostream>
 #include<stack>
 #include<string.h>
