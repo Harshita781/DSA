0464c467ad64aba61b1edd385e82c5bebd278f95
pranjalpandey1800@gmail.com <sharmaharshita431@gmail.com>
2022-12-30 22:48:08 +0530

    initial

--- Queue/DQueue.cpp
+++ Queue/DQueue.cpp
@@ -2,7 +2,52 @@
 #include<iostream>
 #include<queue>
 using namespace std;
-
+class Deque{
+    int *arr;
+    int front;
+    int rear;
+    int size;
+    public:
+    Deque(int n){
+        size=n;
+        arr=new int[n];
+        front=-1;
+        rear=-1;
+    }
+    bool pushElement(int x){
+        if(front==0 && rear==size-1 || (rear==(front-1)%(size-1))){
+            return false;
+        }
+        else if(front==-1){
+             front=rear=0;
+        }
+        else if(front==0){
+            front=n-1;
+        }
+        else{
+            front--;
+        }
+        arr[front]=x;
+        return true;
+    }
+    bool pushRear(int x){
+        if((front==0 && rear==size-1) || (rear==(front-1)%(size-1))){
+            return false;
+        } 
+        else if(front==-1){
+            front=rear=0;
+        }
+        else if(rear==size-1 && front!=0){
+            front=0;
+        }
+        else{
+            rear++;
+        }
+        arr[rear]=x;
+        return true;
+    }
+}
+/*
 int main(){
     deque<int> d;
     d.push_front(12);
@@ -21,3 +66,4 @@ int main(){
     }
     return 0;
 }
+*/
