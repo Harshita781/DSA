281f64cfc4680c36d77ce3bf6ccbd843a1ee8cb7
pranjalpandey1800@gmail.com <sharmaharshita431@gmail.com>
2023-01-18 07:39:26 +0530

    Inorder traversal of a Binary tree

---
+++ Binary Tree/InorderTraversal.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+class node{
+    public:
+    int data;
+    node *left;
+    node *right;
+    node(int d){
+        this->data=d;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+
+void Inorder(node* root){
+    if(root==NULL){
+        return;
+    }
+    Inorder(root->left);
+    cout<<root->data<<" ";
+    Inorder(root->right);
+}
+
+node* buildTree(node* root){
+    cout<<"Enter the data: "<<endl;
+    int data;
+    cin>>data;
+    root=new node(data);
+    if(data==-1){
+        return NULL;
+    }
+    cout<<"Enter data for inserting in left of "<<data<<endl;
+    root->left=buildTree(root->left);
+    cout<<"Enter data for inserting in right of "<<data<<endl;
+    root->right=buildTree(root->right);
+    return root;
+}
+int main(){
+    node* root=NULL;
+    root=buildTree(root);
+    cout<<"Inoerder Traversal : "<<endl;
+    Inorder(root);
+     return 0;
+}
