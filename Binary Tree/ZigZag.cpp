2873e41eff0ccd31df044043de2b95ab0c2503ca
pranjalpandey1800@gmail.com <sharmaharshita431@gmail.com>
2023-01-24 22:36:43 +0530

    Zigzag traversal of a binary tree

--- Binary Tree/ZigZag.cpp
+++ Binary Tree/ZigZag.cpp
@@ -1,19 +1,24 @@
-vector<int> result;
+class Solution{
+    public:
+    //Function to store the zig zag order traversal of tree in a list.
+    vector <int> zigZagTraversal(Node* root)
+    {
+    	vector<int> result;
     if(root==NULL)
         return result;
     queue<Node*> q;
     q.push(root);
     bool leftToRight=true;
-    while(!q.empty*()){
+    while(!q.empty()){
         int size=q.size();
         vector<int> ans(size);
         for(int i=0;i<size;i++){
             Node* frontNode=q.front();
             q.pop();
             int index=leftToRight ? i : size-i-1;
-            ans=[index]=frontNode->data;
+            ans[index]=frontNode->data;
             if(frontNode->left)
-                a.push(frontNode->left);
+                q.push(frontNode->left);
             if(frontNode->right)
                 q.push(frontNode->right);
         }
@@ -23,3 +28,5 @@ vector<int> result;
         }
     }
     return result;
+    }
+};
