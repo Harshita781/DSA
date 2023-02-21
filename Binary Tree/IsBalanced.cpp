6b02d95f42fca3c333ee886b04dbb3ce41444367
pranjalpandey1800@gmail.com <sharmaharshita431@gmail.com>
2023-02-21 18:09:22 +0530

    optimized solution

--- Binary Tree/IsBalanced.cpp
+++ Binary Tree/IsBalanced.cpp
@@ -26,3 +26,31 @@ class Solution{
         }
     }
 };
+//Optimized solution
+/*
+pair<bool,int> IsBalancedFast(Node* root){
+    if(root==NULL){
+        pair<bool,int> p = make_pair(true,0);
+        return p;
+    }
+    pair <int,int> left = IsBalancedFast(root->left);
+    pair <int,int> right = IsBalancedFast(root->right);
+
+    bool leftAns=left.first;
+    bool rightAns=right.first;
+    bool diff=abs(left.second-right.second) <= 1;
+
+    pair<int,int> ans;
+    ans.second=max(left.second,right.second)+10;
+    if(left && right && diff){
+        ans.first = true;
+    }
+    else{
+        ans.first = false;
+    }
+    return ans;
+}
+bool IsBalanced(Node* root){
+    return IsBalancedFast(root).first;
+}
+*/
