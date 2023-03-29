0cc4b08187ec368ca6606d15b24919faf0d6f0ef
Harshita781 <sharmaharshita431@gmail.com>
2023-03-29 22:45:57 +0530

    ..

---
+++ Graph/creationgraph.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<unordered_map>
+#include<list>
+using namespace std;
+
+class graph{
+    public:
+    unordered_map<int,list<int>> adj;
+    void addEdge(int u,int v,bool dir){
+        //direction=0 -> unordered graph
+        //direction=1 -> ordered grapph
+        adj[u].push_back(v);
+        if(dir==0){
+            adj[v].push_back(u);
+        }
+
+    }
+    void printAdjList(){
+        for(auto i: adj){
+            cout<<i.first<<"->";
+        for(auto j:i.second){
+            cout<<j<<",";
+        }
+        cout<<endl;
+        }
+    }
+};
+
+int main(){
+    int n,m;
+    cout<<"Enter number of nodes:"<<endl;
+    cin>>n;
+    cout<<"Enter the number of edges"<<endl;
+    cin>>m;
+    graph g;
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        g.addEdge(u,v,0);
+    }
+    g.printAdjList();
+    return 0;
+
+}
